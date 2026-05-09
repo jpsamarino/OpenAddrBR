18a2d58eb8ad82427e737027949c55639a3cee8d
jpsamarino <jpsamarino3@hotmail.com>
2026-05-09 13:13:31 -0400

    refactor: remove sys.path.insert from utils

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,10 +1,5 @@
 """Compatibility shim - re-export from openaddrbr.utils."""
 
-import sys
-from pathlib import Path
-
-sys.path.insert(0, str(Path(__file__).parent))
-
 from openaddrbr.utils import (
     text_similarity,
     find_best_street_match,
