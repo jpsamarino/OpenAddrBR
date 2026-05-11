e124e3472d99d739101209cb1ebb788be6a19c10
jpsamarino <jpsamarino3@hotmail.com>
2026-05-10 21:41:26 -0400

    refactor: remove utils/ wrapper - use openaddrbr.utils directly

--- utils/similarity.py
+++
@@ -1,6 +0,0 @@
-"""Compatibility - use openaddrbr.utils."""
-
-from openaddrbr.utils._matching import make_similarity_func, text_similarity
-from openaddrbr.utils._text import text_to_ascii
-
-__all__ = ["text_similarity", "make_similarity_func", "text_to_ascii"]
