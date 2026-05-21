fe724fbca1c708852237b170bfa89555b304ddfe
jpsamarino <jpsamarino3@hotmail.com>
2026-05-21 15:16:43 -0400

    refactor: use star imports in data/__init__.py

--- openaddrbr/data/__init__.py
+++ openaddrbr/data/__init__.py
@@ -1,28 +1,7 @@
 """Data package - path configuration and vector search."""
 
-from openaddrbr.core._env import (
-    get_data_path,
-    get_model_path,
-    get_sgeodb_path,
-    get_usearch_dir,
-    set_data_path,
-)
-from openaddrbr.data._data_download import check_data_exists, download_data
-from openaddrbr.data._sql_db import SQLDB
-from openaddrbr.data._tantivy import TantivySearch
-from openaddrbr.data._usearch import UsearchIndex, get_semantic_index, search_vector
-
-__all__ = [
-    "get_data_path",
-    "set_data_path",
-    "get_sgeodb_path",
-    "get_usearch_dir",
-    "get_model_path",
-    "check_data_exists",
-    "download_data",
-    "get_semantic_index",
-    "search_vector",
-    "UsearchIndex",
-    "SQLDB",
-    "TantivySearch",
-]
+from openaddrbr.core._env import *
+from openaddrbr.data._data_download import *
+from openaddrbr.data._sql_db import *
+from openaddrbr.data._tantivy import *
+from openaddrbr.data._usearch import *
