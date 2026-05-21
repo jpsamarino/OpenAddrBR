83080e485bc8da6719347b795bb82e0d5c0967fe
jpsamarino <jpsamarino3@hotmail.com>
2026-05-21 10:58:46 -0400

    feat: move Encoder to services/_encoder.py
    Move from core/_encoder.py to services/. Update services/__init__.py exports.

--- openaddrbr/services/__init__.py
+++ openaddrbr/services/__init__.py
@@ -2,8 +2,11 @@
 
 from openaddrbr.services._cep import is_multi_street_cep, search_by_cep
 from openaddrbr.services._city import get_city_info
+from openaddrbr.services._encoder import VALID_BACKENDS, Encoder
 
 __all__ = [
+    "Encoder",
+    "VALID_BACKENDS",
     "get_city_info",
     "search_by_cep",
     "is_multi_street_cep",
